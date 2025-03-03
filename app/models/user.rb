c694f2218c3b694f288170f9a1f07a77b5d3b9fb
Washington Arambillete <milanghdarkoh@hotmail.com>
2025-03-03 18:51:56 +0100

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # campos basicos para usuarios basicos o cliente
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+
+  # campos para usuarios administradores
+  attribute :is_admin, :boolean, default: false
 end

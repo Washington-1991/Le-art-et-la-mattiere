23c1ea16bb863e02d73da44b2477e31cef1573a7
Washington Arambillete <milanghdarkoh@hotmail.com>
2025-03-18 12:47:57 +0100

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get "articles/index"
   devise_for :users
   root to: "pages#home"
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :articles
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/*
@@ -15,7 +13,4 @@ Rails.application.routes.draw do
 
   # Nueva ruta para /presentation
   get "presentation", to: "pages#presentation"
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end

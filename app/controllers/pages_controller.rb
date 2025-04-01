65216b4e39fcfd20b931efa93d27d107be5ca048
Washington Arambillete <milanghdarkoh@hotmail.com>
2025-04-01 12:20:56 +0200

    wip

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,16 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
+  before_action :authorize_admin, only: [:admin_dashboard]
+
+  def admin_dashboard
+    # Renderiza la vista solo si es admin
+  end
+
+  private
+
+  def authorize_admin
+    redirect_to root_path, alert: "No tienes acceso a esta página." unless current_user.admin?
+  end
 
   def home
   end

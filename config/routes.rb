b6e87a2f2ae6999b9a539a0cad4130545645b75a
Washington Arambillete <milanghdarkoh@hotmail.com>
2025-04-04 23:12:32 +0200

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
+  get "/admin", to: "admin#index"
 
   resources :users, only: [:show]  # Solo permite `show` para todos
 

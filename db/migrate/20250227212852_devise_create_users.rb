b27558ad6b3dd01d261afbb223353b056f18b4b6
Washington Arambillete <milanghdarkoh@hotmail.com>
2025-04-07 12:26:38 +0200

    wip

--- db/migrate/20250227212852_devise_create_users.rb
+++ db/migrate/20250227212852_devise_create_users.rb
@@ -5,40 +5,48 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.2]
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :encrypted_password,  null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
+      t.datetime :reset_password_token_expires_at  # Nueva columna para expiración de token
 
       ## Rememberable
       t.datetime :remember_created_at
+      t.string   :remember_token  # Token adicional para remember me
 
-      ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
+      ## Trackable (importante para seguridad)
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
-      ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      ## Confirmable (verificación de email)
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email
 
-      ## Lockable
-      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
+      ## Lockable (protección contra fuerza bruta)
+      t.integer  :failed_attempts, default: 0, null: false
+      t.string   :unlock_token
+      t.datetime :locked_at
 
+      ## Seguridad adicional
+      t.datetime :password_changed_at  # Para invalidar tokens cuando cambia la contraseña
+      t.string   :otp_secret_key  # Para autenticación de dos factores
+      t.boolean  :otp_required, default: false  # Habilitar 2FA
 
       t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    add_index :users, :confirmation_token,   unique: true
+    add_index :users, :unlock_token,         unique: true
+    add_index :users, :remember_token,       unique: true
+    add_index :users, :password_changed_at   # Para auditoría
   end
 end

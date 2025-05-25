d5cba592efec26198cc8cae45435435d5c2a449e
Washington Arambillete <milanghdarkoh@hotmail.com>
2025-05-25 14:08:40 +0200

    wip

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,8 @@ class Cart < ApplicationRecord
   belongs_to :user
   has_many :cart_items
   has_many :articles, through: :cart_items
+
+  def calculate_total
+    cart_items.sum('quantity * price')
+  end
 end

c7db56dc4d54169a13d19e7fb5b711156d7752c4
Washington Arambillete <milanghdarkoh@hotmail.com>
2025-07-17 21:54:37 +0200

    wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,19 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     resource.admin? ? admin_dashboard_path : root_path
   end
+
+  helper_method :current_cart
+
+  def current_cart
+    if session[:cart_id]
+      Cart.find(session[:cart_id])
+    else
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+    end
+  rescue ActiveRecord::RecordNotFound
+    session[:cart_id] = nil
+    current_cart
+  end
 end

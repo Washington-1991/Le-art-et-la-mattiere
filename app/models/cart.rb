e198c43a8e2c82269c5d24f49847d788b2025f58
Washington Arambillete <milanghdarkoh@hotmail.com>
2025-07-18 12:42:51 +0200

    wip

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,6 +3,17 @@ class Cart < ApplicationRecord
   has_many :cart_items
   has_many :articles, through: :cart_items
 
+  def add_article(article_id)
+  existing_item = cart_items.find_by(article_id: article_id)
+
+  if existing_item
+    existing_item.quantity += 1
+    existing_item.save
+  else
+    article = Article.find(article_id)
+    cart_items.create(article: article, quantity: 1, price: article.price)
+  end
+
   def calculate_total
     cart_items.sum('quantity * price')
   end

8191505363f00fbb23f55dec60f6398f19f042b2
Washington Arambillete <milanghdarkoh@hotmail.com>
2025-07-18 12:59:24 +0200

    wip

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,15 +4,16 @@ class Cart < ApplicationRecord
   has_many :articles, through: :cart_items
 
   def add_article(article_id)
-  existing_item = cart_items.find_by(article_id: article_id)
+    existing_item = cart_items.find_by(article_id: article_id)
 
-  if existing_item
-    existing_item.quantity += 1
-    existing_item.save
-  else
-    article = Article.find(article_id)
-    cart_items.create(article: article, quantity: 1, price: article.price)
-  end
+    if existing_item
+      existing_item.quantity += 1
+      existing_item.save
+    else
+      article = Article.find(article_id)
+      cart_items.create(article: article, quantity: 1, price: article.price)
+    end
+  end  # ← ESTE end faltaba para cerrar add_article
 
   def calculate_total
     cart_items.sum('quantity * price')

96581f3cfa4d94affe8067902da3e1a6e78ab59b
Washington Arambillete <milanghdarkoh@hotmail.com>
2025-07-19 22:32:43 +0200

    wip

--- test/controllers/cart_items_controller_test.rb
+++ test/controllers/cart_items_controller_test.rb
@@ -2,14 +2,18 @@ class CartItemsController < ApplicationController
   before_action :set_cart
 
   def create
-    article = Article.find(params[:article_id])
-    @cart.add_article(article.id)
-    redirect_to cart_path, notice: 'Produit ajouté au panier.'
+    @article = Article.find(params[:article_id])
+
+    if @cart.add_article(@article.id)
+      redirect_to cart_path, notice: 'Produit ajouté au panier.'
+    else
+      redirect_to @article, alert: "Impossible d'ajouter l'article au panier"
+    end
   end
 
   def destroy
-    cart_item = @cart.cart_items.find(params[:id])
-    cart_item.destroy
+    @cart_item = @cart.cart_items.find(params[:id])
+    @cart_item.destroy
     redirect_to cart_path, notice: 'Article supprimé du panier.'
   end
 

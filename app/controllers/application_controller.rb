5c01ccb004799e0166e2f08c35bf89305af1afa6
Washington Arambillete <milanghdarkoh@hotmail.com>
2025-07-19 22:24:39 +0200

    wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,16 +7,19 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_cart
 
+  private
+
   def current_cart
     if session[:cart_id]
-      Cart.find(session[:cart_id])
+      Cart.find_by(id: session[:cart_id]) || create_cart
     else
-      cart = Cart.create
-      session[:cart_id] = cart.id
-      cart
+      create_cart
     end
-  rescue ActiveRecord::RecordNotFound
-    session[:cart_id] = nil
-    current_cart
+  end
+
+  def create_cart
+    cart = Cart.create(user: current_user) # Asocia el carrito al usuario si existe
+    session[:cart_id] = cart.id
+    cart
   end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,17 +3,28 @@ class Cart < ApplicationRecord
   has_many :cart_items
   has_many :articles, through: :cart_items
 
-  def add_article(article_id)
+    def add_article(article_id)
+    article = Article.find(article_id)
+
+    if article.stock <= 0
+      errors.add(:base, "Cet article est en rupture de stock")
+      return false
+    end
+
     existing_item = cart_items.find_by(article_id: article_id)
 
     if existing_item
       existing_item.quantity += 1
       existing_item.save
     else
-      article = Article.find(article_id)
-      cart_items.create(article: article, quantity: 1, price: article.price)
+      cart_items.create(
+        article: article,
+        quantity: 1,
+        price: article.price
+      )
     end
-  end  # ← ESTE end faltaba para cerrar add_article
+    true
+  end # ← ESTE end faltaba para cerrar add_article
 
   def calculate_total
     cart_items.sum('quantity * price')

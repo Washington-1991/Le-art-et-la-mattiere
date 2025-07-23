a6ab57872a45cee88da4376fceff27bd0b4fd394
Washington Arambillete <milanghdarkoh@hotmail.com>
2025-07-23 20:35:58 +0200

    wip

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,8 @@
 class CartsController < ApplicationController
+  # carts_controller.rb
   def show
-    @cart = current_cart
+    @cart = current_user.carts.last # or whatever logic you use to find the current cart
+    # or if you have a way to mark a cart as "current":
+    # @cart = current_user.carts.where(status: 'active').first
   end
 end

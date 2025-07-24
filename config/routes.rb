d63b5a5a3b142fb991afaa7593399ccb34e155da
Washington Arambillete <milanghdarkoh@hotmail.com>
2025-07-24 12:46:41 +0200

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # Página principal
   root to: "pages#home"
   get '/home', to: 'pages#home', as: :home
 
@@ -6,7 +7,7 @@ Rails.application.routes.draw do
   get 'categories/:category', to: 'articles#category', as: :category_articles
   resources :articles
 
-  # Users (Devise)
+  # Usuarios (Devise)
   devise_for :users
   resources :users, only: [:show]
 
@@ -18,18 +19,15 @@ Rails.application.routes.draw do
   get "/admin", to: "admin#index"
   get "admin_dashboard", to: "pages#admin_dashboard", as: :admin_dashboard
 
-  # Cart - Simplificado y mejorado
-  resources :cart_items, only: [:create, :destroy]
-  resource :cart, only: [:show]  # Esto crea cart_path (singular)
-
-  # Elimina esta línea redundante:
-  # get "carts/show"
+  # Carrito
+  resources :cart_items, only: [:create, :update, :destroy]  # <- se añadió :update aquí
+  resource :cart, only: [:show]  # singular porque cada usuario tiene un solo carrito
 
   # PWA
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Misc
+  # Páginas varias
   get "presentation", to: "pages#presentation"
   get "up" => "rails/health#show", as: :rails_health_check
 end

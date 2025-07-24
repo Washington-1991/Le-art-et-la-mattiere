272fca4f515951cd3d24258f00a69fd9f57a381e
Washington Arambillete <milanghdarkoh@hotmail.com>
2025-07-24 12:20:58 +0200

    wip

--- test/controllers/cart_items_controller_test.rb
+++ test/controllers/cart_items_controller_test.rb
@@ -1,27 +1,33 @@
 class CartItemsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_cart
 
   def create
-    @cart = current_user.cart || Cart.create(user: current_user)
     article = Article.find(params[:article_id])
-    @cart_item = CartItem.new(cart: @cart, article: article)
+    @cart_item = @cart.cart_items.find_or_initialize_by(article: article)
+
+    if @cart_item.persisted?
+      @cart_item.increment!(:quantity)
+    else
+      @cart_item.quantity = 1
+    end
 
     if @cart_item.save
-      redirect_to cart_path(@cart), notice: "Article ajouté au panier."
+      redirect_to cart_path, notice: "#{article.name} a été ajouté à votre panier"
     else
-      redirect_to article_path(article), alert: "Erreur lors de l'ajout au panier."
+      redirect_to article_path(article), alert: "Impossible d'ajouter l'article: #{@cart_item.errors.full_messages.to_sentence}"
     end
   end
 
   def destroy
-    @cart_item = CartItem.find(params[:id])
+    @cart_item = @cart.cart_items.find(params[:id])
     @cart_item.destroy
-    redirect_to cart_path, notice: 'Article supprimé du panier.'
+    redirect_to cart_path, notice: 'Article supprimé du panier'
   end
 
   private
 
   def set_cart
-    @cart = current_cart
+    @cart = current_user.cart || current_user.create_cart
   end
 end

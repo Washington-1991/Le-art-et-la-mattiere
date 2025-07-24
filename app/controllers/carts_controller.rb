9c4e8656587d4fd32e7ebafad694b799953ec097
Washington Arambillete <milanghdarkoh@hotmail.com>
2025-07-24 21:43:24 +0200

    wip

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,9 +1,9 @@
 class CartsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_cart, only: [:show, :checkout]
+  before_action :set_current_cart, only: [:show, :checkout]
 
   def show
-    # @cart ya está establecido por el before_action
+    # @current_cart ya está establecido por el before_action
     respond_to do |format|
       format.html
       format.turbo_stream
@@ -11,24 +11,18 @@ class CartsController < ApplicationController
   end
 
   def checkout
-    if @cart.cart_items.empty?
+    if @current_cart.cart_items.empty?
       redirect_to cart_path, alert: "Votre panier est vide"
       return
     end
-
-    # Lógica adicional para preparar el pago podría incluir:
-    @total = @cart.total_price
-    @cart_items = @cart.cart_items.includes(:article)
-
-    # Aquí podrías inicializar algún objeto relacionado con el pago
-    # @payment = Payment.new(amount: @total, user: current_user)
+    @total = @current_cart.total_price
+    @cart_items = @current_cart.cart_items.includes(:article)
   end
 
   private
 
-  def set_cart
-    @cart = current_user.cart || current_user.create_cart
-    # Alternativa más robusta para manejar múltiples carritos:
-    # @cart = current_user.carts.in_progress.first || current_user.carts.create
+  def set_current_cart
+    # Obtiene el carrito más reciente o crea uno nuevo
+    @current_cart = current_user.carts.order(created_at: :desc).first || current_user.carts.create
   end
 end

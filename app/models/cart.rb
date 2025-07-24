e9de1351898dadfcc9b6edc4014b0715570ef2d0
Washington Arambillete <milanghdarkoh@hotmail.com>
2025-07-24 23:01:24 +0200

    wip

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,18 +1,10 @@
-class CartsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_current_cart
+# app/models/cart.rb
+class Cart < ApplicationRecord
+  belongs_to :user
+  has_many   :cart_items, dependent: :destroy
 
-  def show
-    # Lógica básica sin estado
-    respond_to do |format|
-      format.html
-      format.turbo_stream
-    end
-  end
-
-  private
-
-  def set_current_cart
-    @current_cart = current_user.carts.order(created_at: :desc).first || current_user.carts.create
+  # Calcula el total sumando precio×cantidad de cada ítem
+  def total_price
+    cart_items.sum('price * quantity')
   end
 end
